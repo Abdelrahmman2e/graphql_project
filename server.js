5e50a8968eda608f2b8777582846fbc8fe3a6cab
abdomostafa2e <your_email@example.com>
2025-10-21 08:42:16 +0300

    Setup basic GraphQL server

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const { buildSchema } = require("graphql");
+const { createHandler } = require("graphql-http/lib/use/express");
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const schema = buildSchema(`
+  type Query{
+  Hello :String
+  }
+  `);
 
+const resolvers = {
+  Hello: () => "Hello from server GraphQL",
+};
+
+const app = express();
+
+app.use("/graphql", createHandler({schema, rootValue:resolvers}));
+
+const port = 3000;
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Server app listening on port ${port}`);
+});
